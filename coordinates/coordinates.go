94a0ac1e94040d5b6aeaa86e6206f34566e21996
Julien Levasseur <Julien Levasseur>
2023-02-11 08:56:03 -0500

    Add coordinates pkg

---
+++ coordinates/coordinates.go
@@ -0,0 +1,35 @@
+package coordinates
+
+import (
+	"math"
+
+	"github.com/umahmood/haversine"
+)
+
+func RoundTo1Decimal(coords haversine.Coord) haversine.Coord {
+	return haversine.Coord{
+		lat: math.Round(coords.Lat*10) / 10,
+		lon: math.Round(coords.Lon*10) / 10,
+	}
+}
+
+func RoundTo2Decimals(coords haversine.Coord) haversine.Coord {
+	return haversine.Coord{
+		lat: math.Round(coords.Lat*100) / 100,
+		lon: math.Round(coords.Lon*100) / 100,
+	}
+}
+
+func RoundTo3Decimals(coords haversine.Coord) haversine.Coord {
+	return haversine.Coord{
+		lat: math.Round(coords.Lat*1000) / 1000,
+		lon: math.Round(coords.Lon*1000) / 1000,
+	}
+}
+
+func RoundTo5Decimals(coords haversine.Coord) haversine.Coord {
+	return haversine.Coord{
+		lat: math.Round(coords.Lat*10000) / 10000,
+		lon: math.Round(coords.Lon*10000) / 10000,
+	}
+}
---
+++ coordinates/go.mod
@@ -0,0 +1,5 @@
+module github.com/julienlevasseur/gps/coordinates
+
+go 1.19
+
+require github.com/umahmood/haversine v0.0.0-20151105152445-808ab04add26
