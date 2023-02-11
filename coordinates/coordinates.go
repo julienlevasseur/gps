742cb68cc8530915028301c7ac3761245a17e3a5
Julien Levasseur <Julien Levasseur>
2023-02-11 09:33:22 -0500

    Fix lat/lon fields

--- coordinates/coordinates.go
+++ coordinates/coordinates.go
@@ -8,28 +8,28 @@ import (
 
 func RoundTo1Decimal(coords haversine.Coord) haversine.Coord {
 	return haversine.Coord{
-		lat: math.Round(coords.Lat*10) / 10,
-		lon: math.Round(coords.Lon*10) / 10,
+		Lat: math.Round(coords.Lat*10) / 10,
+		Lon: math.Round(coords.Lon*10) / 10,
 	}
 }
 
 func RoundTo2Decimals(coords haversine.Coord) haversine.Coord {
 	return haversine.Coord{
-		lat: math.Round(coords.Lat*100) / 100,
-		lon: math.Round(coords.Lon*100) / 100,
+		Lat: math.Round(coords.Lat*100) / 100,
+		Lon: math.Round(coords.Lon*100) / 100,
 	}
 }
 
 func RoundTo3Decimals(coords haversine.Coord) haversine.Coord {
 	return haversine.Coord{
-		lat: math.Round(coords.Lat*1000) / 1000,
-		lon: math.Round(coords.Lon*1000) / 1000,
+		Lat: math.Round(coords.Lat*1000) / 1000,
+		Lon: math.Round(coords.Lon*1000) / 1000,
 	}
 }
 
 func RoundTo4Decimals(coords haversine.Coord) haversine.Coord {
 	return haversine.Coord{
-		lat: math.Round(coords.Lat*10000) / 10000,
-		lon: math.Round(coords.Lon*10000) / 10000,
+		Lat: math.Round(coords.Lat*10000) / 10000,
+		Lon: math.Round(coords.Lon*10000) / 10000,
 	}
 }
